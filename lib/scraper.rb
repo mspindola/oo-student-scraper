755dce924115ef951b451d4bf89dfa22dd98bfe6
Moises <moises.spindola@gmail.com>
2021-02-05 12:33:37 -0800

    commit tree 6fd17a772bc01de4a1d3f199af87f71cbb526da7

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,46 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    
+  def self.scrape_index_page(index_url) # is a class method that scrapes the student index page ('./fixtures/student-site/index.html') and a returns an array of hashes in which each hash represents one student
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |rostercards| #iterates over the index page
+      rostercards.css(".student-card a").each do |student| #iterates inside each student card
+        student_profile_link = "#{student.attr('href')}" #work on this?? 
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
+    end  
+    students
+    #binding.pry
   end
-
+      
   def self.scrape_profile_page(profile_url)
+    student = {}
+    profile_page = Nokogiri::HTML(open(profile_url))
+    links = profile_page.css(".social-icon-container").children.css("a").map { |i| i.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link 
+      else
+        student[:blog] = link
+      end
+    end
+      student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+      student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
     
+    student
   end
-
 end
 
+# Scraper
+#   #scrape_index_page
+#     is a class method that scrapes the student index page ('./fixtures/student-site/index.html') and a returns an array of hashes in which each hash represents one student
+#   #scrape_profile_page
+#     is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student
+#     can handle profile pages without all of the social links
--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
@@ -5,19 +7,45 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    @@all << self    
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all
   end
 end
+
+# ##Scraper
+#   #scrape_index_page
+#   is a class method that scrapes the student index page ('./fixtures/student-site/index.html') and a returns an array of hashes in which each hash represents one student
+#   #scrape_profile_page
+#     is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student
+#     can handle profile pages without all of the social links
+
+# Student
+#   #new
+#     takes in an argument of a hash and sets that new student's attributes using the key/value pairs of that hash.
+#     adds that new student to the Student class' collection of all existing students, stored in the `@@all` class variable.
+#   .create_from_collection
+#     uses the Scraper class to create new students with the correct name and location.
+#   #add_student_attributes
+#     uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student.
+#   .all
+#     returns the class variable @@all
 
